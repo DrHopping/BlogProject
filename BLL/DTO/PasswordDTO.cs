cc032b5e6e73ab0de9916f1f62bc8e4a8da66fbe
Никита Педоренко <nikitnya1@gmail.com>
2020-03-11 21:58:30 +0200

    Create PasswordDTO.cs

---
+++ BLL/DTO/PasswordDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BLL.DTO
+{
+    class PasswordDTO
+    {
+        [RegularExpression(@"^.*(?=.{6,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(^[a-zA-Z0-9@\$=!:.#%]+$)")]
+        public string OldPassword { get; set; }
+        [RegularExpression(@"^.*(?=.{6,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(^[a-zA-Z0-9@\$=!:.#%]+$)")]
+        public string NewPassword { get; set; }
+    }
+}
