183818e4c02a3f742cd0ae93f83e1ca1da4c2e17
Никита Педоренко <nikitnya1@gmail.com>
2020-03-11 21:06:56 +0200

    Added mappers base

---
+++ BLL/Interfaces/IMapper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace BLL.Interfaces
+{
+    public interface IMappper<TFirst, TSecond>
+    {
+        TFirst Map(TSecond element);
+        TSecond Map(TFirst element);
+        ICollection<TFirst> Map(ICollection<TSecond> elements);
+        ICollection<TSecond> Map(ICollection<TFirst> elements);
+    }
+}
---
+++ BLL/Mappers/ArticleMapper.cs
@@ -0,0 +1,18 @@
+﻿using BLL.DTO;
+using DAL.Entities;
+
+namespace BLL.Mappers
+{
+    public class ArticleMapper : BaseMapper<Article, ArticleDto>
+    {
+        public override Article Map(ArticleDto element)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override ArticleDto Map(Article element)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
