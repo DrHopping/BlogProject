92416de964bb88e63d5404f35a310bab5cc72242
Никита Педоренко <nikitnya1@gmail.com>
2020-03-11 22:07:44 +0200

    Create BlogMapper.cs

---
+++ BLL/Mappers/BlogMapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BLL.DTO;
+using DAL.Entities;
+
+namespace BLL.Mappers
+{
+    public class BlogMapper : BaseMapper<BlogDTO, Blog>
+    {
+        public override BlogDTO Map(Blog element)
+        {
+            return new BlogDTO
+            {
+                BlogId = element.BlogId,
+                Name = element.Name,
+                OwnerId = element.OwnerId,
+            };
+        }
+
+        public override Blog Map(BlogDTO element)
+        {
+            return new Blog
+            {
+                Name = element.Name
+            };
+        }
+    }
+}
