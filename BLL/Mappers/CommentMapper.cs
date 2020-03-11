c183eb0e35fe9c73b07ad0392ad6d1912b944a8a
Никита Педоренко <nikitnya1@gmail.com>
2020-03-11 22:11:18 +0200

    Create CommentMapper.cs

---
+++ BLL/Mappers/CommentMapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BLL.DTO;
+using DAL.Entities;
+
+namespace BLL.Mappers
+{
+    public class CommentMapper : BaseMapper<Comment, CommentDTO>
+    {
+        public override Comment Map(CommentDTO element)
+        {
+            return new Comment
+            {
+                Content = element.Content,
+                ArticleId = element.ArticleId.GetValueOrDefault()
+            };
+        }
+
+        public override CommentDTO Map(Comment element)
+        {
+            return new CommentDTO
+            {
+                CommentId = element.CommentId,
+                Content = element.Content,
+                ArticleId = element.ArticleId,
+                CreatorId = element.UserId
+            };
+        }
+    }
+}
