745f0eddd8eecf8c6e97f2e408ef4835f2f13a1d
Никита Педоренко <nikitnya1@gmail.com>
2020-03-11 21:57:47 +0200

    Create DTOs
    Article, Blog, Comment, Tag

---
+++ BLL/DTO/ArticleDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BLL.DTO
+{
+    public class ArticleDTO
+    {
+        public int? ArticleId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int? BlogId { get; set; }
+        public string AuthorId { get; set; }
+        public string AuthorUsername { get; set; }
+        public ICollection<CommentDTO> Comments { get; set; }
+        public ICollection<TagDTO> Tags { get; set; }
+    }
+}
---
+++ BLL/DTO/BlogDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.DTO
+{
+    class BlogDTO
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public string OwnerId { get; set; }
+        public string OwnerUsername { get; set; }
+        public IEnumerable<ArticleDTO> Articles { get; set; }
+    }
+}
