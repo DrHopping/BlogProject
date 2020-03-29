75fbd5e020737521fd3c5688a50ab2dc74f2b5fd
Никита Педоренко <nikitnya1@gmail.com>
2020-03-29 19:09:40 +0300

    Update AccountService.cs

--- BLL/Services/AccountService.cs
+++ BLL/Services/AccountService.cs
@@ -89,5 +89,75 @@ namespace BLL.Services
                     throw new NotEnoughRightsException();
             }
         }
+
+        public async Task<bool> DeleteUser(string id, string token)
+        {
+            if (id == null) throw new ArgumentNullException(nameof(id));
+            if (token == null) throw new ArgumentNullException(nameof(token));
+
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null) throw new ArgumentNullException(nameof(user), $"Couldn't find user with id {id}");
+
+            var requesterId = _jwtFactory.GetUserIdClaim(token);
+            if (requesterId == id) return (await _userManager.DeleteAsync(user)).Succeeded;
+
+            var requesterRoleClaim = _jwtFactory.GetUserRoleClaim(token);
+            if (requesterRoleClaim == "Admin")
+            {
+                return (await _userManager.DeleteAsync(user)).Succeeded;
+            }
+            throw new NotEnoughRightsException();
+        }
+
+        public async Task<bool> UpdateUser(string id, UserDTO user, string token)
+        {
+            if (user == null) throw new ArgumentNullException(nameof(user));
+            if (user.UserName == null && user.Email == null) throw new ArgumentNullException(nameof(user));
+
+            var requesterId = _jwtFactory.GetUserIdClaim(token);
+            var requesterRole = _jwtFactory.GetUserRoleClaim(token);
+
+            var userEntity = await _userManager.FindByIdAsync(id);
+            if (userEntity == null) throw new ArgumentNullException(nameof(userEntity), $"Couldn't find user with id {id}");
+
+            if (requesterId == id || requesterRole == "Admin")
+            {
+                if (user.UserName != null && userEntity.UserName.CompareTo(user.UserName) != 0)
+                {
+                    var isNameTaken = await _userManager.FindByNameAsync(user.UserName);
+                    if (isNameTaken != null) throw new NameAlreadyTakenException();
+                    userEntity.UserName = user.UserName;
+                }
+                else if (user.Email != null && userEntity.Email.CompareTo(user.Email) != 0)
+                {
+                    var isEmailTaken = await _userManager.FindByEmailAsync(user.Email);
+                    if (isEmailTaken != null) throw new NameAlreadyTakenException();
+                    userEntity.Email = user.Email;
+                }
+
+                return (await _userManager.UpdateAsync(userEntity)).Succeeded;
+            }
+
+            throw new NotEnoughRightsException();
+        }
+
+        public async Task<bool> ChangePassword(string id, PasswordDTO password, string token)
+        {
+            if (password == null) throw new ArgumentNullException(nameof(password));
+            if (password.NewPassword == null) throw new ArgumentNullException(nameof(password.NewPassword));
+            if (password.OldPassword == null) throw new ArgumentNullException(nameof(password.OldPassword));
+
+            var requesterId = _jwtFactory.GetUserIdClaim(token);
+            var requesterRole = _jwtFactory.GetUserRoleClaim(token);
+            var userEntity = await _userManager.FindByIdAsync(id);
+            if (userEntity == null) throw new ArgumentNullException(nameof(userEntity), $"Couldn't find user with id {id}");
+
+            if (requesterId != id && requesterRole != "Admin") throw new NotEnoughRightsException();
+
+            bool checkPassword = await _userManager.CheckPasswordAsync(userEntity, password.OldPassword);
+            if (checkPassword == false) throw new ArgumentException(nameof(password));
+            return (await _userManager.ChangePasswordAsync(userEntity, password.OldPassword, password.NewPassword)).Succeeded;
+
+        }
     }
 }
