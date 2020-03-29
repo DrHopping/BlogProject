61e6d4d401b60690fefe416aa1881656b97926ff
Никита Педоренко <nikitnya1@gmail.com>
2020-03-29 20:03:32 +0300

    Update AuthService.cs

--- BLL/Services/AuthService.cs
+++ BLL/Services/AuthService.cs
@@ -1,4 +1,9 @@
-﻿using BLL.Interfaces;
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using BLL.DTO;
+using BLL.Exceptions;
+using BLL.Interfaces;
 using BLL.Models;
 using DAL.Entities;
 using Microsoft.AspNetCore.Identity;
@@ -10,5 +15,41 @@ namespace BLL.Services
         private UserManager<User> _userManager;
         private IJwtFactory _jwtFactory;
         private JwtOptions _jwtIssuerOptions;
+
+        public AuthService(UserManager<User> userManager, IJwtFactory jwtFactory, JwtOptions jwtIssuerOptions)
+        {
+            _userManager = userManager;
+            _jwtFactory = jwtFactory;
+            _jwtIssuerOptions = jwtIssuerOptions;
+        }
+
+        public async Task<ClaimsIdentity> GetClaimsIdentity(UserDTO user)
+        {
+            if (user == null) throw new ArgumentNullException(nameof(user));
+            if (user.UserName == null) throw new ArgumentNullException(nameof(user.UserName));
+            if (user.Password == null) throw new ArgumentNullException(nameof(user.Password));
+
+            var userToVerify = await _userManager.FindByNameAsync(user.UserName);
+            if (userToVerify == null)
+            {
+                userToVerify = await _userManager.FindByEmailAsync(user.UserName);
+                if (userToVerify == null)
+                {
+                    throw new WrongCredentialsException();
+                }
+            }
+
+            if (await _userManager.CheckPasswordAsync(userToVerify, user.Password))
+            {
+                return await _jwtFactory.GenerateClaimsIdentity(userToVerify);
+            }
+
+            if (await _userManager.CheckPasswordAsync(userToVerify, user.Password))
+            {
+                return await _jwtFactory.GenerateClaimsIdentity(userToVerify);
+            }
+
+            throw new WrongCredentialsException();
+        }
     }
 }
