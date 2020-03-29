1981330f119280673943fa0a61a1e5d3895c4ff1
Никита Педоренко <nikitnya1@gmail.com>
2020-03-29 19:46:48 +0300

    Create AuthService.cs

---
+++ BLL/Services/AuthService.cs
@@ -0,0 +1,14 @@
+﻿using BLL.Interfaces;
+using BLL.Models;
+using DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace BLL.Services
+{
+    public class AuthService
+    {
+        private UserManager<User> _userManager;
+        private IJwtFactory _jwtFactory;
+        private JwtOptions _jwtIssuerOptions;
+    }
+}
