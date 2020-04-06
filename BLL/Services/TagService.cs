29e067ad8115ce05b0837d5b3f4c293ce45a0d29
Никита Педоренко <nikitnya1@gmail.com>
2020-04-06 13:33:02 +0300

    Updated TagService to work with AutoMapper

--- BLL/Services/TagService.cs
+++ BLL/Services/TagService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
 using BLL.DTO;
 using BLL.Mappers;
+using DAL.Entities;
 using DAL.Interfaces;
 
 namespace BLL.Services
@@ -10,25 +12,24 @@ namespace BLL.Services
     public class TagService : ITagService
     {
         private readonly IUnitOfWork _unitOfWork;
-        private TagMapper _tagMapper;
-
-        public TagService(IUnitOfWork unitOfWork)
+        private readonly IMapper _mapper;
+        public TagService(IUnitOfWork unitOfWork, IMapper mapper)
         {
             _unitOfWork = unitOfWork;
+            _mapper = mapper;
         }
 
-        private TagMapper TagMapper => _tagMapper ??= new TagMapper();
 
         public async Task<IEnumerable<TagDTO>> GetAllTags()
         {
-            return TagMapper.Map(await _unitOfWork.TagRepository.Get());
+            return _mapper.Map<IEnumerable<Tag>, IEnumerable<TagDTO>>(await _unitOfWork.TagRepository.Get());
         }
 
         public async Task<TagDTO> GetTagById(int id)
         {
             var tag = await _unitOfWork.TagRepository.GetByIdAsync(id);
             if (tag == null) throw new ArgumentNullException(nameof(tag));
-            return TagMapper.Map(tag);
+            return _mapper.Map<Tag, TagDTO>(tag);
         }
     }
 }
