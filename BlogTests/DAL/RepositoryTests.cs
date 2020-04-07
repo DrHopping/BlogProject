f7639cb2ddb33b7df905657df39b0bc3ca31c75a
Никита Педоренко <nikitnya1@gmail.com>
2020-04-07 22:58:24 +0300

    Fixed existing tests

--- BlogTests/DAL/RepositoryTests.cs
+++ BlogTests/DAL/RepositoryTests.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using BLL.Mappings;
+using BLL.Services;
 using Xunit;
 using Moq;
 using DAL.Data;
 using DAL.Entities;
+using DAL.Interfaces;
 using DAL.Repositories;
+using DAL.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
+using MockQueryable.Moq;
+using Moq.EntityFrameworkCore;
 
 namespace BlogTests.DAL
 {
@@ -16,101 +25,76 @@ namespace BlogTests.DAL
         [Fact]
         public void Insert_SuccessfullyCreateBlog()
         {
+            //Arrange
             var mockSet = new Mock<DbSet<Blog>>();
-
             var mockContext = new Mock<BlogDbContext>();
             mockContext.Setup(m => m.Set<Blog>()).Returns(mockSet.Object);
-
             var repo = new GenericRepository<Blog>(mockContext.Object);
-
-            var blog = new Blog()
-            {
-                Name = "Blog",
-            };
+            var blog = new Blog { Name = "Blog" };
+            //Act
             repo.Insert(blog);
-
+            //Assert
             mockSet.Verify(m => m.Add(It.IsAny<Blog>()), Times.Once);
         }
 
         [Fact]
-        public void Get_WithNoArguments_ReturnsAllBlogs()
+        public async Task Get_WithNoArguments_ReturnsAllBlogs()
         {
+            //Arrange
             var data = new List<Blog>
             {
                 new Blog{Name = "BBB"},
                 new Blog{Name = "ZZZ"},
                 new Blog{Name = "AAA"}
-            }.AsQueryable();
-
-            var mockSet = new Mock<DbSet<Blog>>();
-            mockSet.As<IQueryable<Blog>>().Setup(m => m.Provider).Returns(data.Provider);
-            mockSet.As<IQueryable<Blog>>().Setup(m => m.Expression).Returns(data.Expression);
-            mockSet.As<IQueryable<Blog>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            mockSet.As<IQueryable<Blog>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
-
+            }.AsQueryable().BuildMockDbSet();
             var mockContext = new Mock<BlogDbContext>();
-            mockContext.Setup(c => c.Set<Blog>()).Returns(mockSet.Object);
-
+            mockContext.Setup(c => c.Set<Blog>()).Returns(data.Object);
             var repo = new GenericRepository<Blog>(mockContext.Object);
-
-            var blogs = repo.Get();
-
+            //Act
+            var blogs = await repo.Get();
+            //Assert
             Assert.Equal(3, blogs.Count());
         }
 
         [Fact]
-        public void Get_WithFilter_ReturnsSuitableValues()
+        public async Task Get_WithFilter_ReturnsSuitableValues()
         {
+            //Arrange
             var data = new List<Blog>
             {
                 new Blog{Name = "BB"},
                 new Blog{Name = "ZZZ"},
                 new Blog{Name = "AAAA"}
-            }.AsQueryable();
-
-            var mockSet = new Mock<DbSet<Blog>>();
-            mockSet.As<IQueryable<Blog>>().Setup(m => m.Provider).Returns(data.Provider);
-            mockSet.As<IQueryable<Blog>>().Setup(m => m.Expression).Returns(data.Expression);
-            mockSet.As<IQueryable<Blog>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            mockSet.As<IQueryable<Blog>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
-
+            }.AsQueryable().BuildMockDbSet();
             var mockContext = new Mock<BlogDbContext>();
-            mockContext.Setup(c => c.Set<Blog>()).Returns(mockSet.Object);
-
+            mockContext.Setup(c => c.Set<Blog>()).Returns(data.Object);
             var repo = new GenericRepository<Blog>(mockContext.Object);
-
-            var blogs = repo.Get(b => b.Name.Length > 2);
-
+            //Act
+            var blogs = await repo.Get(b => b.Name.Length > 2);
             Assert.Equal(2, blogs.Count());
         }
 
         [Fact]
-        public void Get_WithOrderBy_ReturnsSortedValues()
+        public async Task Get_WithOrderBy_ReturnsSortedValues()
         {
+            //Arrange
             var data = new List<Blog>
             {
                 new Blog{Name = "BBB"},
                 new Blog{Name = "ZZZ"},
                 new Blog{Name = "AAA"}
-            }.AsQueryable();
-
-            var mockSet = new Mock<DbSet<Blog>>();
-            mockSet.As<IQueryable<Blog>>().Setup(m => m.Provider).Returns(data.Provider);
-            mockSet.As<IQueryable<Blog>>().Setup(m => m.Expression).Returns(data.Expression);
-            mockSet.As<IQueryable<Blog>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            mockSet.As<IQueryable<Blog>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
-
+            }.AsQueryable().BuildMockDbSet();
             var mockContext = new Mock<BlogDbContext>();
-            mockContext.Setup(c => c.Set<Blog>()).Returns(mockSet.Object);
-
+            mockContext.Setup(c => c.Set<Blog>()).Returns(data.Object);
             var repo = new GenericRepository<Blog>(mockContext.Object);
-
-            var blogs = repo.Get(orderBy: q => q.OrderBy(b => b.Name));
-
+            //Act
+            var blogs = await repo.Get(orderBy: q => q.OrderBy(b => b.Name));
+            //Assert
             Assert.Equal(3, blogs.Count());
             Assert.Equal("AAA", blogs.ElementAt(0).Name);
             Assert.Equal("BBB", blogs.ElementAt(1).Name);
             Assert.Equal("ZZZ", blogs.ElementAt(2).Name);
         }
+
     }
 }
