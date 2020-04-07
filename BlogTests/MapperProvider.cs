c4c8e44cfd160086ae100ef6e8af491e91a8fd56
Никита Педоренко <nikitnya1@gmail.com>
2020-04-07 22:34:11 +0300

    Create MapperProvider.cs

---
+++ BlogTests/MapperProvider.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BLL.Mappings;
+
+namespace BlogTests
+{
+    internal class MapperProvider
+    {
+        public static IMapper GetMapper()
+        {
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+            return new Mapper(configuration);
+        }
+    }
+}
