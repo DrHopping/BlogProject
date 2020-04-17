00acbd1649f78dc566088c878dea0ac2c8a59ec8
Никита Педоренко <nikitnya1@gmail.com>
2020-04-17 18:17:34 +0300

    Create EntityNotFoundException.cs

---
+++ DAL/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BLL.Exceptions
+{
+    public class EntityNotFoundException : ArgumentException
+    {
+        public override string Message { get; }
+
+        public EntityNotFoundException(string type, int id) : base()
+        {
+            Message = $"Couldn't find {type} with id {id}";
+        }
+
+    }
+}
