f4c6c49a7aca997475b8d3546b6569ea0dfefe5d
Никита Педоренко <nikitnya1@gmail.com>
2020-04-17 21:03:13 +0300

    Update BlogService.cs

--- BLL/Services/BlogService.cs
+++ BLL/Services/BlogService.cs
@@ -6,7 +6,6 @@ using AutoMapper;
 using BLL.DTO;
 using BLL.Exceptions;
 using BLL.Interfaces;
-using BLL.Mappers;
 using DAL.Entities;
 using DAL.Interfaces;
 
@@ -36,34 +35,24 @@ namespace BLL.Services
         public async Task<BlogDTO> CreateBlog(BlogDTO blog, string token)
         {
             if (blog == null) throw new ArgumentNullException(nameof(blog));
+            if (await _unitOfWork.BlogRepository.FirstOrDefaultAsync(b => b.Name == blog.Name) != null) throw new NameAlreadyTakenException();
 
             var claimsId = _jwtFactory.GetUserIdClaim(token);
             var blogEntity = _mapper.Map<BlogDTO, Blog>(blog);
             blogEntity.OwnerId = claimsId;
 
-            _unitOfWork.BlogRepository.Insert(blogEntity);
-            await _unitOfWork.SaveAsync();
+            var result = await _unitOfWork.BlogRepository.InsertAndSaveAsync(blogEntity);
 
-            blogEntity = (await _unitOfWork.BlogRepository
-                .Get(b => b.Name == blog.Name || b.OwnerId == blog.OwnerId))
-                .FirstOrDefault();
-            if (blogEntity == null) throw new ArgumentNullException(nameof(blogEntity), "Couldn't create blog");
-
-            var result = _mapper.Map<Blog, BlogDTO>(blogEntity);
-            return result;
+            return _mapper.Map<Blog, BlogDTO>(result);
         }
 
         public async Task DeleteBlog(int id, string token)
         {
             if (token == null) throw new ArgumentNullException(nameof(token));
             var blogEntity = await _unitOfWork.BlogRepository.GetByIdAsync(id);
-            if (blogEntity == null) throw new ArgumentNullException(nameof(blogEntity), "This blog doesn't exist");
-            if (CheckRights(token, blogEntity.OwnerId))
-            {
-                _unitOfWork.BlogRepository.Delete(blogEntity);
-                _unitOfWork.Save();
-            }
-            else throw new NotEnoughRightsException();
+            if (!CheckRights(token, blogEntity.OwnerId))
+                throw new NotEnoughRightsException();
+            await _unitOfWork.BlogRepository.DeleteAndSaveAsync(blogEntity);
         }
 
         public async Task UpdateBlogName(int id, BlogDTO blog, string token)
@@ -72,28 +61,23 @@ namespace BLL.Services
             if (blog == null) throw new ArgumentNullException(nameof(blog));
 
             var entity = await _unitOfWork.BlogRepository.GetByIdAsync(id);
-            if (entity == null) throw new ArgumentNullException(nameof(entity), "This blog doesn't exist");
 
             if (!CheckRights(token, entity.OwnerId)) throw new NotEnoughRightsException();
-            if ((await _unitOfWork.BlogRepository.Get(b => b.Name == blog.Name)).FirstOrDefault() != null) throw new NameAlreadyTakenException();
+            if (await _unitOfWork.BlogRepository.FirstOrDefaultAsync(b => b.Name == blog.Name) != null) throw new NameAlreadyTakenException();
 
             entity.Name = blog.Name;
-            _unitOfWork.BlogRepository.Update(entity);
-            await _unitOfWork.SaveAsync();
+            await _unitOfWork.BlogRepository.UpdateAndSaveAsync(entity);
         }
 
         public async Task<BlogDTO> GetBlogById(int id)
         {
-            var blog = (await _unitOfWork.BlogRepository.Get((b => b.Id == id), includeProperties: "Articles,Owner")).FirstOrDefault();
-            if (blog == null) throw new ArgumentNullException(nameof(blog), "Couldn't find blog by id");
-
-            var dto = _mapper.Map<Blog, BlogDTO>(blog);
-            return dto;
+            var blog = await _unitOfWork.BlogRepository.GetByIdAsync(id, "Articles,Owner");
+            return _mapper.Map<Blog, BlogDTO>(blog);
         }
 
         public async Task<IEnumerable<BlogDTO>> GetAllBlogs()
         {
-            return _mapper.Map<IEnumerable<Blog>, IEnumerable<BlogDTO>>(await _unitOfWork.BlogRepository.Get());
+            return _mapper.Map<IEnumerable<Blog>, IEnumerable<BlogDTO>>(await _unitOfWork.BlogRepository.GetAllAsync());
         }
 
         public async Task<IEnumerable<ArticleDTO>> GetAllArticlesByBlogId(int id)
