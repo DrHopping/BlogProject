d289b898307d6429d0bcdaab09bacbe50f7bcc4e
Никита Педоренко <nikitnya1@gmail.com>
2020-04-21 12:45:37 +0300

    Create RequestExtensions.cs

---
+++ Blog/Extensions/RequestExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Net.Http.Headers;
+
+namespace Blog.Extensions
+{
+    public static class RequestExtensions
+    {
+        public static string GetToken(this HttpRequest request)
+        {
+            var accessToken = request.Headers[HeaderNames.Authorization].ToString().Split(' ')[1];
+            if (accessToken == null) throw new ArgumentNullException("Couldn't get the token user authorized with");
+            return accessToken;
+        }
+    }
+}
